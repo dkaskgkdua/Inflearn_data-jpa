a0751c9b70bc84252cf077e6fbd3da1b57c1734f
SongMinJun <dkaskgkdua@gmail.com>
2021-04-26 23:49:35 +0900

    사용자 정의 리포지토리 구현
    - 관례 : 기존리포지토리이름 + Impl
    - 화면중심의 통계쿼리가 모인 repository와 핵심 비즈니스가 들어간 repository 분리

---
+++ src/main/java/study/datajpa/repository/MemberQueryRepository.java
@@ -0,0 +1,24 @@
+package study.datajpa.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import study.datajpa.entity.Member;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+/**
+ * 핵심 비즈니스가 들어간 repository와
+ * 화면에 맞춰진 api를 분리(복잡한 쿼리들...)
+ */
+@Repository
+@RequiredArgsConstructor
+public class MemberQueryRepository {
+
+    private final EntityManager em;
+
+    List<Member> findAllMembers() {
+        return em.createQuery("select m from Member m")
+                .getResultList();
+    }
+}
